5438c2768da6c80c723adf41626f674d9b7edf2d
marwat98 <marc.wat001@gmail.com>
2025-06-03 22:06:26 +0200

    first commit

---
+++ src/main/java/InformationClass/Sentences.java
@@ -0,0 +1,10 @@
+package InformationClass;
+
+/**
+ * class contain constructor which get string sentence
+ */
+public class Sentences {
+    public void mainSentence(String comunicate){
+        System.out.println(comunicate);
+    }
+}
---
+++ src/main/java/srcMailSentence/Main.java
@@ -0,0 +1,24 @@
+package srcMailSentence;
+
+import InformationClass.Sentences;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Sentences showSentence = new Sentences();
+
+        showSentence.mainSentence("-------Mail_Sentence_Program-------");
+        showSentence.mainSentence("1.Wpisz email: ");
+        scanner.hasNext();
+
+
+
+
+
+
+
+
+    }
+}
