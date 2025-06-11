8dd0337dbc999a0b4ab0fe88505c0f04b0851832
marwat98 <marc.wat001@gmail.com>
2025-06-11 20:43:54 +0200

    Changed program structure and begining create possibility on write edit inputs mails

--- src/main/java/MainProgramFile/Main.java
+++ src/main/java/MainProgramFile/Main.java
@@ -4,6 +4,7 @@ import GmailSendMessage.GmailSend;
 import InformationClass.Sentences;
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -11,20 +12,45 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Sentences showSentence = new Sentences();
         EmailValidator email = EmailValidator.getInstance();
+        File myEmailFile = new File("src/main/java/ProgramFiles/myEmailFile.txt");
+        boolean isValid;
 
+        showSentence.mainSentence("");
         showSentence.mainSentence("-------Mail_Sentence_Program-------");
-        showSentence.mainSentence("1.Wpisz email: ");
-        String input = scanner.nextLine();
+        showSentence.mainSentence("Menu");
+        showSentence.mainSentence("1.Set email send");
+        showSentence.mainSentence("2.Set email call");
+        showSentence.mainSentence("-----------------------------------");
+        System.out.print("Choose: ");
+        int choose = scanner.nextInt();
+
+        switch(choose){
+            case 1 ->{
+               do{
+                   isValid = true;
+                   showSentence.mainSentence("-----------------------------------");
+                   showSentence.mainSentence(" Choose option in your email file  ");
+                   showSentence.mainSentence("-----------------------------------");
+                   showSentence.mainSentence("1. Write your email");
+                   showSentence.mainSentence("2. Show your email file");
+                   showSentence.mainSentence("-----------------------------------");
+                   System.out.print("Choose: ");
+                   String writeEmail = scanner.next();
+                   String checkWriteEmail = (email.isValid(writeEmail)) ? "Sucess!! Your email is right" : "You write wrong email try again";
+                   System.out.println(checkWriteEmail);
+
+               } while(!isValid);
+
+
+            }
+            case 2 ->{
+
+            }
+        }
+
         GmailSend gmail = new GmailSend("yourmail@gmail.com","sendmail@gmail.com","hostAdress","appPassword");
 
 
-        if(email.isValid(input)){
-            gmail.gmailPuts();
-            gmail.sendMail();
-        } else {
-            showSentence.mainSentence("Podany tekst nie jest mailem");
-
-        }
 
 
 
