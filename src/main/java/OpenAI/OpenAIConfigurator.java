1c130eca9c4388d77fcc028c4936f4904622e81e
marwat98 <marc.wat001@gmail.com>
2025-07-20 22:38:33 +0200

    OpenAIConfigurator with method which generate title for email

---
+++ src/main/java/OpenAI/OpenAIConfigurator.java
@@ -0,0 +1,32 @@
+package OpenAI;
+
+import Interfaces.OpenAIInterface;
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.ChatModel;
+import com.openai.models.chat.completions.ChatCompletion;
+import com.openai.models.chat.completions.ChatCompletionCreateParams;
+
+
+public class OpenAIConfigurator implements OpenAIInterface {
+
+    /**
+     * method contain settings which generate message
+     * @param text is instruction for GPT
+     * @return String with answer
+     */
+    @Override
+    public String generate(String text){
+        OpenAIClient client = OpenAIOkHttpClient.fromEnv();
+        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
+                .addUserMessage(text)
+                .model(ChatModel.GPT_4_1)
+                .build();
+        ChatCompletion chatCompletion = client.chat().completions().create(params);
+        String response = chatCompletion.choices().getFirst().message().content().orElse("Something is wrong try again");
+
+        return response;
+
+    }
+
+}
